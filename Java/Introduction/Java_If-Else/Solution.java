8ad4981d8370bfdda309f0288f78762defc47341
axel <axdavax93@gmail.com>
2026-02-09 18:02:17 -0500

    fixing throws java.io.IOException

--- Java/Introduction/Java_If-Else/Solution.java
+++ Java/Introduction/Java_If-Else/Solution.java
@@ -1,9 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bufferedReader.readLine().trim());
 
