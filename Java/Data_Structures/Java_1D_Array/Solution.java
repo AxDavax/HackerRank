ee988ae3ca0ecfcd093ed0da8f19907a370a1be0
axel <axdavax93@gmail.com>
2026-02-10 11:36:46 -0500

    Data Structures - 1D Array

---
+++ Java/Data_Structures/Java_1D_Array/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int[] a = new int[n];
+
+        for(int i = 0; i<n; i++)
+            a[i] = scan.nextInt();
+
+        for (int value : a)
+            System.out.println(value);
+    }
+}
