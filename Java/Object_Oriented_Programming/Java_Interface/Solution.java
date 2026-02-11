5839752138e0a9aa79ec9740299f05a98d103399
axel <axdavax93@gmail.com>
2026-02-11 10:52:32 -0500

    OOP - Interface

---
+++ Java/Object_Oriented_Programming/Java_Interface/Solution.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+interface AdvancedArithmetic {
+    int divisor_sum(int n);
+}
+
+class MyCalculator implements AdvancedArithmetic {
+    public int divisor_sum(int n) {
+        if(n > 1000) n = 1000;
+
+        int somme = n;
+        for(int i = 1; i <= n/2; i++)
+            if(n % i == 0) somme += i;
+
+        return somme;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        MyCalculator my_calculator = new MyCalculator();
+        System.out.print("I implemented: ");
+        ImplementedInterfaceNames(my_calculator);
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(my_calculator.divisor_sum(n) + "\n");
+        sc.close();
+    }
+
+    static void ImplementedInterfaceNames(Object o) {
+        Class[] theInterfaces = o.getClass().getInterfaces();
+        for (Class theInterface : theInterfaces) {
+            String interfaceName = theInterface.getName();
+            System.out.println(interfaceName);
+        }
+    }
+}
