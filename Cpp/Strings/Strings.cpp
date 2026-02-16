3316d31fd509e610c0b2d58ad83e50855184e469
axel <axdavax93@gmail.com>
2026-02-15 21:50:18 -0500

    Adding Strings - Strings

---
+++ Cpp/Strings/Strings.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string a, b;
+	std::cin >> a;
+	std::cin >> b;
+
+	std::cout << a.size() << " " << b.size() << std::endl;
+
+	std::cout << a + b << std::endl;
+
+	char temp = a[0];
+	a[0] = b[0];
+	b[0] = temp;
+
+	std::cout << a << " " << b << std::endl;
+
+	return 0;
+}
