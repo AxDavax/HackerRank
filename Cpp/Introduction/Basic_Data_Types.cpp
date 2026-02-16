64fdbfde5c838c3d19a8f6caa22a6951e8ac36fc
axel <axdavax93@gmail.com>
2026-02-15 20:20:46 -0500

    Adding Introduction - Basic Data Types

---
+++ Cpp/Introduction/Basic_Data_Types.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() 
+{
+	int x;
+	long y;
+	char z;
+	float w;
+	double t;
+
+	std::cin >> x >> y >> z >> w >> t;
+
+	std::cout << x << std::endl;
+	std::cout << y << std::endl;
+	std::cout << z << std::endl;
+	std::cout << w << std::endl;
+	std::cout << y << std::endl;
+
+	return 0;
+}
