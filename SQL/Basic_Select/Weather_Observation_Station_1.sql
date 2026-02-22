c2622faa3acf528205f86a3d649df6c9f913462d
axel <axdavax93@gmail.com>
2026-02-21 22:56:56 -0500

    Adding Basic Select - Weather Observation Station

---
+++ SQL/Basic_Select/Weather_Observation_Station_1.sql
@@ -0,0 +1,11 @@
+-- ============================================
+-- Problem: Weather Observation Station 1
+-- Platform: HackerRank
+-- Subdomain: Basic Select
+-- Database: MS SQL Server
+-- ============================================
+
+SELECT 
+    CITY, STATE
+FROM
+    STATION;
