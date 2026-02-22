57ddda2b8f8036a00ef3e9e94111395977298bdd
axel <axdavax93@gmail.com>
2026-02-21 22:14:52 -0500

    Adding Basic Select - Select All

---
+++ SQL/Basic_Select/Select_All.sql
@@ -0,0 +1,11 @@
+-- ============================================
+-- Problem: Select All
+-- Platform: HackerRank
+-- Subdomain: Basic Select
+-- Database: MS SQL Server
+-- ============================================
+
+SELECT 
+    *
+FROM 
+    CITY;
