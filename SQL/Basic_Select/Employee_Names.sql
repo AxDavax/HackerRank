a582f41db55cbda7126217a36879e0cb9eadaa0c
axel <axdavax93@gmail.com>
2026-02-22 00:11:38 -0500

    Adding Basic Select - Employee Names

---
+++ SQL/Basic_Select/Employee_Names.sql
@@ -0,0 +1,13 @@
+-- ============================================
+-- Problem: Employee Names
+-- Platform: HackerRank
+-- Subdomain: Basic Select
+-- Database: MS SQL Server
+-- ============================================
+
+SELECT 
+    name
+FROM 
+    Employee
+ORDER BY
+    name;
