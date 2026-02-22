1799ee698fd1c468a7aadc4819286fc67321372a
axel <axdavax93@gmail.com>
2026-02-21 22:24:47 -0500

    Adding Basic Select - Japanese Cities Names

---
+++ SQL/Basic_Select/Japanese_Cities_Names.sql
@@ -0,0 +1,13 @@
+-- ============================================
+-- Problem: Japanese Cities' Names
+-- Platform: HackerRank
+-- Subdomain: Basic Select
+-- Database: MS SQL Server
+-- ============================================
+
+SELECT 
+    NAME
+FROM 
+    CITY
+WHERE
+    COUNTRYCODE = 'JPN';
