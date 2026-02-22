90c6cb98aeb99087c71b73f9a894d8599383930f
axel <axdavax93@gmail.com>
2026-02-21 22:21:21 -0500

    Adding Basic Select - Japanese Cities Attributes

---
+++ SQL/Basic_Select/Japanese_Cities_Attributes.sql
@@ -0,0 +1,13 @@
+-- ============================================
+-- Problem: Japanese Cities' Attributes
+-- Platform: HackerRank
+-- Subdomain: Basic Select
+-- Database: MS SQL Server
+-- ============================================
+
+SELECT 
+    *
+FROM 
+    CITY
+WHERE
+    COUNTRYCODE = 'JPN';
